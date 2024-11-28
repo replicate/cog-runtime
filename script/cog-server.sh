45d488160fb1a7dadd7b7d6f22c18295ad194133
Neville Li <neville.lyh@gmail.com>
2024-11-28 14:37:58 -0500

    Fix cog-server.sh

--- script/cog-server.sh
+++ script/cog-server.sh
@@ -11,15 +11,25 @@ fi
 
 module="$1"
 shift
+
 base_dir="$(git rev-parse --show-toplevel)"
 
-export LOG_FORMAT=development
-export PATH="$base_dir/python/.venv/bin:$PATH"
-export PYTHONPATH="$base_dir/python"
-rm -rf tmp
-mkdir -p tmp
-go run cmd/cog-server/main.go \
-    --working-dir tmp \
-    --module-name "tests.runners.$module" \
-    --class-name Predictor \
-    "$@"
+# PYTHON=1 to run with legacy Cog
+if [ -z "${PYTHON:-}" ]; then
+    export LOG_FORMAT=development
+    export PATH="$base_dir/python/.venv/bin:$PATH"
+    export PYTHONPATH="$base_dir/python/coglet/_compat:$base_dir/python"
+    rm -rf tmp
+    mkdir -p tmp
+    go run cmd/cog-server/main.go \
+        --working-dir tmp \
+        --module-name "tests.runners.$module" \
+        --class-name Predictor \
+        "$@"
+else
+    python_version="$(cat "$base_dir/python/.python-version")"
+    cd "$base_dir/python/tests/runners"
+    cp "$module.py" predict.py
+    trap "rm -f predict.py" EXIT
+    uv run --python "$python_version" --with cog python3 -m cog.server.http "$@"
+fi
