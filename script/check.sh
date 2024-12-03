77f8105a707ebf22c2125ff849608b8376ce3aaf
Neville Li <neville.lyh@gmail.com>
2024-12-02 21:06:31 -0500

    Split go & python CI

--- script/check.sh
+++ script/check.sh
@@ -6,23 +6,47 @@ set -euo pipefail
 
 base_dir="$(git rev-parse --show-toplevel)"
 
-cd "$base_dir"
-local="$(go list -m)"
-if [[ -z "${CI:-}" ]]; then
-    go run golang.org/x/tools/cmd/goimports@latest -d -w -local "$local" .
-else
-    output="$(go run golang.org/x/tools/cmd/goimports@latest -d -local "$local" .)"
-    printf "%s" "$output"
-    [ -z "$output" ] || exit 1
-fi
+check_go() {
+    cd "$base_dir"
+    local="$(go list -m)"
+    if [[ -z "${CI:-}" ]]; then
+        go run golang.org/x/tools/cmd/goimports@latest -d -w -local "$local" .
+    else
+        output="$(go run golang.org/x/tools/cmd/goimports@latest -d -local "$local" .)"
+        printf "%s" "$output"
+        [ -z "$output" ] || exit 1
+    fi
+}
 
-cd "$base_dir/python"
-uv sync --all-extras
-if [[ -z "${CI:-}" ]]; then
-    uv tool run ruff check --fix
-    uv tool run ruff format
+check_python() {
+    cd "$base_dir/python"
+    uv sync --all-extras
+    if [[ -z "${CI:-}" ]]; then
+        uv tool run ruff check --fix
+        uv tool run ruff format
+    else
+        uv tool run ruff check
+        uv tool run ruff format --check
+    fi
+    .venv/bin/mypy . --exclude tests/runners --exclude tests/schemas
+}
+
+if [ $# -eq 0 ]; then
+    check_go
+    check_python
 else
-    uv tool run ruff check
-    uv tool run ruff format --check
+    for c in "$@"; do
+        case "$c" in
+            go)
+                check_go
+                ;;
+            python)
+                check_python
+                ;;
+            *)
+                echo "Unknown check $c"
+                exit 1
+                ;;
+        esac
+    done
 fi
-.venv/bin/mypy . --exclude tests/runners --exclude tests/schemas
