c2bd505f5ec8a1f0a0ca12c411dfffdc89a882d4
Neville Li <neville.lyh@gmail.com>
2024-12-02 21:06:31 -0500

    Split go & python CI

--- script/test.sh
+++ script/test.sh
@@ -7,8 +7,33 @@ set -euo pipefail
 base_dir="$(git rev-parse --show-toplevel)"
 
 cd "$base_dir"
-go test ./...
 
-cd "$base_dir/python"
-uv sync --all-extras
-.venv/bin/pytest
+test_go() {
+    go test ./...
+}
+
+test_python() {
+    cd "$base_dir/python"
+    uv sync --all-extras
+    .venv/bin/pytest
+}
+
+if [ $# -eq 0 ]; then
+    test_go
+    test_python
+else
+    for c in "$@"; do
+        case "$c" in
+            go)
+                test_go
+                ;;
+            python)
+                test_python
+                ;;
+            *)
+                echo "Unknown test $c"
+                exit 1
+                ;;
+        esac
+    done
+fi
