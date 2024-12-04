c114fb48bf20890e9bffd50ce0dc875374f738b5
Neville Li <neville.lyh@gmail.com>
2024-12-04 09:27:45 -0500

    Add test for Path input/output

--- python/tests/runners/path.py
+++ python/tests/runners/path.py
@@ -6,7 +6,9 @@ from cog import BasePredictor, Path
 class Predictor(BasePredictor):
     def predict(self, p: Path) -> Path:
         with open(p, 'r') as f:
+            print('reading input file')
             s = f.read()
         with tempfile.NamedTemporaryFile(mode='w', suffix='.txt', delete=False) as f:
+            print('writing output file')
             f.write(f'*{s}*')
         return Path(f.name)
