55870977dd5a1cdfd466de5b288f3ed63f884dd9
Neville Li <neville.lyh@gmail.com>
2024-12-05 14:09:59 -0500

    Improve webhook debug logging

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -10,9 +10,15 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("===== Request =====")
 		fmt.Println(r.Method, r.URL.Path)
+		for k, v := range r.Header {
+			fmt.Println("----- Headers -----")
+			fmt.Printf("%s: %v\n", k, v)
+		}
 		body := string(must.Get(io.ReadAll(r.Body)))
 		if body != "" {
+			fmt.Println("----- Body -----")
 			fmt.Println(body)
 		}
 		w.WriteHeader(http.StatusOK)
