8961c7db18071d96539bf35f3a5dc5434517b397
Neville Li <neville.lyh@gmail.com>
2024-12-05 19:53:24 -0500

    Fix path_test flakiness

--- python/tests/runners/path.py
+++ python/tests/runners/path.py
@@ -1,4 +1,5 @@
 import tempfile
+import time
 
 from cog import BasePredictor, Path
 
@@ -11,4 +12,5 @@ class Predictor(BasePredictor):
         with tempfile.NamedTemporaryFile(mode='w', suffix='.txt', delete=False) as f:
             print('writing output file')
             f.write(f'*{s}*')
+        time.sleep(0.1)
         return Path(f.name)
