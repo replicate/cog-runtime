1216ff4404ed2d980ec22d58aa506fc6914433c3
Neville Li <neville.lyh@gmail.com>
2024-12-11 14:51:18 -0500

    Handle legacy Cog in iterator_test

--- internal/tests/iterator_test.go
+++ internal/tests/iterator_test.go
@@ -9,36 +9,15 @@ import (
 )
 
 func TestPredictionIteratorSucceeded(t *testing.T) {
-	ct := NewCogTest(t, "iterator")
-	ct.StartWebhook()
-	assert.NoError(t, ct.Start())
-
-	hc := ct.WaitForSetup()
-	assert.Equal(t, server.StatusReady.String(), hc.Status)
-	assert.Equal(t, server.SetupSucceeded, hc.Setup.Status)
-
-	ct.AsyncPrediction(map[string]any{"i": 2, "s": "bar"})
-	wr := ct.WaitForWebhookResponses()
-	logs := ""
-	ct.AssertResponse(wr[0], server.PredictionStarting, nil, logs)
-	logs += "starting prediction\n"
-	ct.AssertResponse(wr[1], server.PredictionProcessing, nil, logs)
-	logs += "prediction in progress 1/2\n"
-	ct.AssertResponse(wr[2], server.PredictionProcessing, nil, logs)
-	ct.AssertResponse(wr[3], server.PredictionProcessing, []any{"*bar-0*"}, logs)
-	logs += "prediction in progress 2/2\n"
-	ct.AssertResponse(wr[4], server.PredictionProcessing, []any{"*bar-0*"}, logs)
-	ct.AssertResponse(wr[5], server.PredictionProcessing, []any{"*bar-0*", "*bar-1*"}, logs)
-	logs += "completed prediction\n"
-	ct.AssertResponse(wr[6], server.PredictionProcessing, []any{"*bar-0*", "*bar-1*"}, logs)
-	ct.AssertResponse(wr[7], server.PredictionSucceeded, []any{"*bar-0*", "*bar-1*"}, logs)
-
-	ct.Shutdown()
-	assert.NoError(t, ct.Cleanup())
+	testPredictionIteratorSucceeded(t, "iterator")
 }
 
 func TestPredictionConcatenateIteratorSucceeded(t *testing.T) {
-	ct := NewCogTest(t, "concat_iterator")
+	testPredictionIteratorSucceeded(t, "concat_iterator")
+}
+
+func testPredictionIteratorSucceeded(t *testing.T, module string) {
+	ct := NewCogTest(t, module)
 	ct.StartWebhook()
 	assert.NoError(t, ct.Start())
 
@@ -48,19 +27,36 @@ func TestPredictionConcatenateIteratorSucceeded(t *testing.T) {
 
 	ct.AsyncPrediction(map[string]any{"i": 2, "s": "bar"})
 	wr := ct.WaitForWebhookResponses()
-	logs := ""
-	ct.AssertResponse(wr[0], server.PredictionStarting, nil, logs)
-	logs += "starting prediction\n"
-	ct.AssertResponse(wr[1], server.PredictionProcessing, nil, logs)
-	logs += "prediction in progress 1/2\n"
-	ct.AssertResponse(wr[2], server.PredictionProcessing, nil, logs)
-	ct.AssertResponse(wr[3], server.PredictionProcessing, []any{"*bar-0*"}, logs)
-	logs += "prediction in progress 2/2\n"
-	ct.AssertResponse(wr[4], server.PredictionProcessing, []any{"*bar-0*"}, logs)
-	ct.AssertResponse(wr[5], server.PredictionProcessing, []any{"*bar-0*", "*bar-1*"}, logs)
-	logs += "completed prediction\n"
-	ct.AssertResponse(wr[6], server.PredictionProcessing, []any{"*bar-0*", "*bar-1*"}, logs)
-	ct.AssertResponse(wr[7], server.PredictionSucceeded, []any{"*bar-0*", "*bar-1*"}, logs)
+	if *legacyCog {
+		assert.Len(t, wr, 5)
+		logs := ""
+		// Compat: legacy Cog sends no "starting" event
+		ct.AssertResponse(wr[0], server.PredictionProcessing, nil, logs)
+		ct.AssertResponse(wr[1], server.PredictionProcessing, []any{"*bar-0*"}, logs)
+		ct.AssertResponse(wr[2], server.PredictionProcessing, []any{"*bar-0*", "*bar-1*"}, logs)
+		// Compat: legacy Cog buffers logging?
+		logs += "starting prediction\n"
+		ct.AssertResponse(wr[3], server.PredictionProcessing, []any{"*bar-0*", "*bar-1*"}, logs)
+		logs += "prediction in progress 1/2\n"
+		logs += "prediction in progress 2/2\n"
+		logs += "completed prediction\n"
+		ct.AssertResponse(wr[4], server.PredictionSucceeded, []any{"*bar-0*", "*bar-1*"}, logs)
+	} else {
+		assert.Len(t, wr, 8)
+		logs := ""
+		ct.AssertResponse(wr[0], server.PredictionStarting, nil, logs)
+		logs += "starting prediction\n"
+		ct.AssertResponse(wr[1], server.PredictionProcessing, nil, logs)
+		logs += "prediction in progress 1/2\n"
+		ct.AssertResponse(wr[2], server.PredictionProcessing, nil, logs)
+		ct.AssertResponse(wr[3], server.PredictionProcessing, []any{"*bar-0*"}, logs)
+		logs += "prediction in progress 2/2\n"
+		ct.AssertResponse(wr[4], server.PredictionProcessing, []any{"*bar-0*"}, logs)
+		ct.AssertResponse(wr[5], server.PredictionProcessing, []any{"*bar-0*", "*bar-1*"}, logs)
+		logs += "completed prediction\n"
+		ct.AssertResponse(wr[6], server.PredictionProcessing, []any{"*bar-0*", "*bar-1*"}, logs)
+		ct.AssertResponse(wr[7], server.PredictionSucceeded, []any{"*bar-0*", "*bar-1*"}, logs)
+	}
 
 	ct.Shutdown()
 	assert.NoError(t, ct.Cleanup())
