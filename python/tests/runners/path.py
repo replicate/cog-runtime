eb7ad4df2debeb98de39b466fe8f2970f04f1b16
Neville Li <neville.lyh@gmail.com>
2024-12-11 17:01:44 -0500

    Spread out path.py logging

--- python/tests/runners/path.py
+++ python/tests/runners/path.py
@@ -6,9 +6,11 @@ from cog import BasePredictor, Path
 
 class Predictor(BasePredictor):
     def predict(self, p: Path) -> Path:
+        time.sleep(0.1)
         with open(p, 'r') as f:
             print('reading input file')
             s = f.read()
+        time.sleep(0.5)
         with tempfile.NamedTemporaryFile(mode='w', suffix='.txt', delete=False) as f:
             print('writing output file')
             f.write(f'*{s}*')
