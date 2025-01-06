daf65833e96af3342c1c421e1095b8b07154b4b9
Neville Li <neville.lyh@gmail.com>
2025-01-06 16:56:19 -0500

    Add python3 -m cog.server.http wrapper

--- python/coglet/__init__.py
+++ python/coglet/__init__.py
@@ -1,18 +1,4 @@
-import pathlib
-import sys
-import warnings
-
 try:
     from ._version import __version__
 except ImportError:
     __version__ = '0.0.0+unknown'
-
-warnings.warn(
-    (
-        'coglet/_compat/ is being added to the front of sys.path '
-        "for 'cog' import compatibility"
-    ),
-    category=ImportWarning,
-    stacklevel=2,
-)
-sys.path.insert(0, str(pathlib.Path(__file__).absolute().parent / '_compat'))
