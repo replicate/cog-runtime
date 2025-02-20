2a9f3d6b9f42f3ab4dc6af7ccbb77142f078cf1c
Neville Li <neville.lyh@gmail.com>
2025-02-20 14:21:33 -0500

    Suppress output when generating schema

--- python/coglet/schema.py
+++ python/coglet/schema.py
@@ -10,8 +10,16 @@ def main():
         print(f'Usage {os.path.basename(sys.argv[0])} <MODULE> <CLASS>')
         sys.exit(1)
 
+    # Some libraries print progress upon import and mess up schema JSON
+    _stdout_write = sys.stdout.write
+    _stderr_write = sys.stderr.write
+    sys.stdout.write = lambda out: len(out)
+    sys.stderr.write = lambda out: len(out)
     p = inspector.create_predictor(sys.argv[1], sys.argv[2])
     s = schemas.to_json_schema(p)
+    sys.stdout.write = _stdout_write
+    sys.stderr.write = _stderr_write
+
     print(json.dumps(s, indent=2))
 
 
