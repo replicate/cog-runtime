bfe5ae5c1f6463a9abdb8d8cdf177ce7e27c7e93
Neville Li <neville.lyh@gmail.com>
2025-02-25 17:07:25 -0500

    Handle Output type

---
+++ internal/tests/output_test.go
@@ -0,0 +1,36 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/replicate/cog-runtime/internal/server"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPredictionOutputSucceeded(t *testing.T) {
+	ct := NewCogTest(t, "output")
+	assert.NoError(t, ct.Start())
+
+	hc := ct.WaitForSetup()
+	assert.Equal(t, server.StatusReady.String(), hc.Status)
+	assert.Equal(t, server.SetupSucceeded, hc.Setup.Status)
+
+	resp := ct.Prediction(map[string]any{"p": b64encode("bar")})
+	logs := "reading input file\nwriting output file\n"
+	var b64 string
+	if *legacyCog {
+		// Compat: different MIME type detection logic
+		b64 = b64encodeLegacy("*bar*")
+	} else {
+		b64 = b64encode("*bar*")
+	}
+	output := map[string]any{
+		"path": b64,
+		"text": "*bar*",
+	}
+	ct.AssertResponse(resp, server.PredictionSucceeded, output, logs)
+
+	ct.Shutdown()
+	assert.NoError(t, ct.Cleanup())
+}
