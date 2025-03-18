24f6c0c62ccbe90e860d1f8d732d9c96c849c2d6
Neville Li <neville.lyh@gmail.com>
2025-03-18 18:38:32 -0400

    wip coder

--- python/coglet/util.py
+++ python/coglet/util.py
@@ -1,11 +1,12 @@
+import dataclasses
 import typing
 from datetime import datetime, timezone
-from typing import Any, Tuple
+from typing import Any, Optional, Tuple
 
 from coglet import adt, api
 
 
-def check_cog_type(tpe: type) -> Tuple[adt.Type, bool]:
+def check_cog_type(tpe: type) -> Tuple[adt.Type, bool, Optional[api.Coder]]:
     if typing.get_origin(tpe) is list:
         t_args = typing.get_args(tpe)
         assert len(t_args) == 1, 'list must have one type argument'
@@ -15,14 +16,19 @@ def check_cog_type(tpe: type) -> Tuple[adt.Type, bool]:
         elem_t = tpe
         is_list = False
     cog_t = adt.PYTHON_TO_COG.get(elem_t)
+    coder = None
+    if cog_t is None and dataclasses.is_dataclass(elem_t):
+        cog_t = adt.Type.CUSTOM
+        coder = api.DataclassCoder(elem_t)  # type: ignore
     assert cog_t is not None, f'unsupported Cog type {elem_t}'
-    return cog_t, is_list
+    return cog_t, is_list, coder
 
 
-def check_value(expected: adt.Type, value: Any) -> bool:
+def check_value(expected: adt.Type, value: Any, coder: Optional[api.Coder]) -> bool:
     cog_t = adt.PYTHON_TO_COG.get(type(value))
     if cog_t is None:
-        return False
+        # Unknown Python type, must be custom type with coder
+        return expected is adt.Type.CUSTOM and coder is not None
     elif expected is adt.Type.FLOAT:
         return cog_t in adt.NUMERIC_TYPES
     elif expected is adt.Type.PATH:
@@ -51,6 +57,13 @@ def normalize_value(expected: adt.Type, value: Any) -> Any:
         return api.Secret(value) if type(value) is str else value
     else:
         return value
+
+
+def normalize_input(expected: adt.Type, value: Any, coder: Optional[api.Coder]) -> Any:
+    if expected is adt.Type.CUSTOM and coder is not None:
+        return coder.decode(value)
+    else:
+        return normalize_value(expected, value)
 
 
 def now_iso() -> str:
