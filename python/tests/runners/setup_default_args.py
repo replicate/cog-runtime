122b5bc2a3978c5e67764edf493e0e2f2886d3be
Will Sackfield <sackfield@replicate.com>
2025-04-23 10:24:47 -0400

    Fix lint

--- python/tests/runners/setup_default_args.py
+++ python/tests/runners/setup_default_args.py
@@ -2,9 +2,8 @@ from cog import BasePredictor
 
 
 class Predictor(BasePredictor):
-
     def setup(self, example_arg: bool = False) -> None:
-        print(f"Example Arg: {example_arg}")
+        print(f'Example Arg: {example_arg}')
 
     def predict(self) -> str:
-        return "hello"
+        return 'hello'
