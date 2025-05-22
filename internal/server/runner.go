69ddbe5d82a7f75b45600b8596431c17126dabe3
Nikhil Sinha <nikhil@replicate.com>
2025-05-21 19:10:27 -0700

    Add logs from subprocesses

--- internal/server/runner.go
+++ internal/server/runner.go
@@ -533,8 +533,28 @@ func (r *Runner) log(line string) {
 	} else if !strings.Contains(line, "[coglet]") {
 		r.mu.Lock()
 		defer r.mu.Unlock()
-		r.logs = append(r.logs, line)
-		r.setupResult.Logs = util.JoinLogs(r.logs)
+		if r.setupResult.CompletedAt != "" {
+			// Anything from inside would be a subprocess call. If it's an async
+			// prediction though, we have no clue who's process is who's - this
+			// can lead to us leaking outputs from one user to another so we
+			// shouldn't keep the lines here
+			if len(r.pending) > 1 {
+				log.Errorw("expected only 1 prediction to be running", "num_running", len(r.pending))
+			} else {
+				for pid := range r.pending {
+					pr := r.pending[pid]
+					fmt.Printf("Line: %s\n", line)
+					pr.appendLogLine(line)
+					// In case log is received before "starting" response
+					if pr.response.Status != "" {
+						pr.sendWebhook(WebhookLogs)
+					}
+				}
+			}
+		} else {
+			r.logs = append(r.logs, line)
+			r.setupResult.Logs = util.JoinLogs(r.logs)
+		}
 	}
 	fmt.Println(line)
 }
