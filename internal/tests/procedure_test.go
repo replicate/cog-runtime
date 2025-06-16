2997b1db10a9590f7a22c6de823686ef4f941763
Neville Li <neville.lyh@gmail.com>
2025-06-16 15:33:46 -0400

    Use procedure runner status (#103)

--- internal/tests/procedure_test.go
+++ internal/tests/procedure_test.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -41,15 +43,36 @@ func TestProcedure(t *testing.T) {
 		return ct.prediction(http.MethodPost, "/procedures", req)
 	}
 
-	resp1 := prediction("foo", "bar", map[string]any{"s": "foobar"})
-	assert.Equal(t, server.PredictionSucceeded, resp1.Status)
-	assert.Equal(t, "s=foobar, token=bar", resp1.Output)
-	assert.Contains(t, resp1.Logs, "predicting foo\n")
+	var wg sync.WaitGroup
 
-	resp2 := prediction("bar", "baz", map[string]any{"i": 123456})
-	assert.Equal(t, server.PredictionSucceeded, resp2.Status)
-	assert.Equal(t, "i=123456, token=baz", resp2.Output)
-	assert.Contains(t, resp2.Logs, "predicting bar\n")
+	assert.Equal(t, server.StatusReady.String(), ct.HealthCheck().Status)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		resp1 := prediction("foo", "bar", map[string]any{"s": "foobar"})
+		assert.Equal(t, server.PredictionSucceeded, resp1.Status)
+		assert.Equal(t, "s=foobar, token=bar", resp1.Output)
+		assert.Contains(t, resp1.Logs, "predicting foo\n")
+	}()
+	time.Sleep(500 * time.Millisecond) // Wait for runner startup
+	assert.Equal(t, server.StatusBusy.String(), ct.HealthCheck().Status)
+	wg.Wait()
+
+	// Wait for status reset to ready
+	time.Sleep(100 * time.Millisecond)
+
+	assert.Equal(t, server.StatusReady.String(), ct.HealthCheck().Status)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		resp2 := prediction("bar", "baz", map[string]any{"i": 123456})
+		assert.Equal(t, server.PredictionSucceeded, resp2.Status)
+		assert.Equal(t, "i=123456, token=baz", resp2.Output)
+		assert.Contains(t, resp2.Logs, "predicting bar\n")
+	}()
+	time.Sleep(500 * time.Millisecond) // Wait for runner startup
+	assert.Equal(t, server.StatusBusy.String(), ct.HealthCheck().Status)
+	wg.Wait()
 
 	ct.Shutdown()
 	assert.NoError(t, ct.Cleanup())
--- python/tests/procedures/bar/predict.py
+++ python/tests/procedures/bar/predict.py
@@ -1,7 +1,10 @@
+import time
+
 from cog import current_scope
 
 
 def predict(i: int) -> str:
     print('predicting bar')
+    time.sleep(1)
     token = current_scope().context['replicate_api_token']
     return f'i={i}, token={token}'
