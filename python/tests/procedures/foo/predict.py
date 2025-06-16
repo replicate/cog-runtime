2a2b44a82ac572abac799a858c4c8d0c98a6d8ab
Neville Li <neville.lyh@gmail.com>
2025-06-16 15:22:42 -0400

    Use procedure runner status

--- python/tests/procedures/foo/predict.py
+++ python/tests/procedures/foo/predict.py
@@ -1,7 +1,10 @@
+import time
+
 from cog import current_scope
 
 
 def predict(s: str) -> str:
     print('predicting foo')
+    time.sleep(1)
     token = current_scope().context['replicate_api_token']
     return f's={s}, token={token}'
