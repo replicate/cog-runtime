32265353f41943508fd404bb361a4783f8403121
Neville Li <neville.lyh@gmail.com>
2025-07-07 16:33:09 -0400

    Tweak default=None error message

--- python/tests/bad_predictors/input_default_none.py
+++ python/tests/bad_predictors/input_default_none.py
@@ -5,7 +5,7 @@ from tests.util import check_python_version
 
 check_python_version(min_version=(3, 10))
 
-ERROR = 'input type must be Optional for None default value'
+ERROR = 'input with default=None must be Optional'
 
 
 class Predictor(BasePredictor):
@@ -19,10 +19,10 @@ class Predictor(BasePredictor):
         i1c: int | None,
         i2a: int = Input(),
         i2b: int = Input(default=None),  # Bad
-        i2c: int = Input(default=0),
+        i2c: int = Input(default=1),
         i3a: Optional[int] = Input(),
         i3b: Optional[int] = Input(default=None),
-        i3c: Optional[int] = Input(default=0),
+        i3c: Optional[int] = Input(default=1),
         i4a: int | None = Input(),
         i4b: int | None = Input(default=None),
         i4c: int | None = Input(default=1),
