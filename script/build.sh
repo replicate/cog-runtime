d133487f4d72783d664e2d118a3bbef0cc042a86
Neville Li <neville.lyh@gmail.com>
2025-07-21 13:51:45 -0400

    Fix clet CI

--- script/build.sh
+++ script/build.sh
@@ -12,25 +12,16 @@ rm -rf build
 rm -rf dist
 rm -rf python/cog/cog-*
 
-# Create "clet", coglet lite for web downloading
-# This does not contain the go binaries
-.venv/bin/python3 -m build -w
-for file in dist/coglet-*; do
-  [ -e "$file" ] || continue
-  filename=$(basename "$file")
-  new_filename=${filename/coglet-/clet-}
-  mv "$file" "dist/$new_filename"
-  echo "Renamed: $file -> dist/$new_filename"
-done
-
-# Export Python version to Go
-uv run --with setuptools_scm python3 -m setuptools_scm > internal/util/version.txt
-
-# Binaries are bundled in Python wheel
-for os in darwin linux; do
-    for arch in amd64 arm64; do
-        CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -o python/cog/cog-$os-$arch ./cmd/cog
+# Skip Go binaries if building "clet", i.e. coglet without go for pyodide
+if [ -z "${CLET:-}" ]; then
+    # Export Python version to Go
+    uv run --with setuptools_scm python3 -m setuptools_scm > internal/util/version.txt
+    # Binaries are bundled in Python wheel
+    for os in darwin linux; do
+        for arch in amd64 arm64; do
+            CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -o python/cog/cog-$os-$arch ./cmd/cog
+        done
     done
-done
+fi
 
 .venv/bin/python3 -m build -w
