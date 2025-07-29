1f171472dd6c592e85c05c82299d51552ae10211
Neville Li <neville.lyh@gmail.com>
2025-07-29 18:15:46 -0400

    Copy procedure dir instead of symlink
    Symlinks are not readable if the parent directory of src URL is not readable by the unprivileged user. So copy everything and chown after to avoid that.

--- script/test-setuid.sh
+++ script/test-setuid.sh
@@ -12,19 +12,31 @@ cd "$base_dir"
 ./script/build.sh
 
 name="test-setuid-$(date '+%s')"
+read -r -d '' SCRIPT << EOF || :
+set -e
+
+# Procedure source URLs in production might be readable by root only
+# So symlink won't work after dropping privilege
+cp -r /src/python/tests/procedures /
+chmod 700 /procedures/setuid
+chmod 600 /procedures/setuid/*
+
+find /src/dist -name '*.whl' -exec pip install {} \;
+python3 -m cog.server.http --port $port --use-procedure-mode
+EOF
 docker run -it --rm --detach \
     --name "$name" \
     --entrypoint /bin/bash \
     --publish "$port:$port" \
     --volume "$PWD":/src python:latest \
-    -c "find /src/dist -name '*.whl' -exec pip install {} \; && python3 -m cog.server.http --port $port --use-procedure-mode"
+    -c "$SCRIPT"
 
 sleep 3  # wait for server startup
 resp="$(mktemp)"
 trap 'rm $resp; docker stop $name' EXIT
 curl -fsSL -X POST \
     -H 'Content-Type: application/json' \
-    --data '{"context":{"procedure_source_url": "file:///src/python/tests/procedures/setuid", "replicate_api_token": "token"}, "input":{"s":"hello"}}' \
+    --data '{"context":{"procedure_source_url": "file:///procedures/setuid", "replicate_api_token": "token"}, "input":{"s":"hello"}}' \
     "http://localhost:$port/procedures" > "$resp"
 
 status="$(jq --raw-output '.status' < "$resp")"
