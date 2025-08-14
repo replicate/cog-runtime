54a45bd148f8ada635986f314d29ed8d9b647ff3
Neville Li <neville.lyh@gmail.com>
2025-08-13 21:30:02 -0400

    Test concurrent predictions with drop privilege

--- python/tests/procedures/setuid/predict.py
+++ python/tests/procedures/setuid/predict.py
@@ -1,5 +1,6 @@
 import os
 import tempfile
+import time
 
 from cog import Path
 
@@ -7,13 +8,27 @@ BASE_UID = 9000
 NOGROUP_GID = 65534
 
 
-def predict(p: Path) -> Path:
+def predict(p: Path, i: int) -> Path:
     uid = os.getuid()
     gid = os.getgid()
     print(f'UID={uid}')
     print(f'GID={gid}')
     assert uid >= BASE_UID
     assert gid == NOGROUP_GID
+
+    if i == 0:
+        # First runner, create a file in a hard-coded path
+        with open('/tmp/out.txt', 'w') as f:
+            f.write('out')
+        # Make it slow to simulate concurrent predictions & runners
+        time.sleep(1)
+    else:
+        # Wait for the first prediction to write the file
+        time.sleep(1)
+        # Second runner, check that we do not own the file
+        stat = os.stat('/tmp/out.txt')
+        assert stat.st_uid != uid
+        assert stat.st_gid == gid
 
     # CWD is a copy of the procedure source
     # where all directories and files are owned by UID/GID
