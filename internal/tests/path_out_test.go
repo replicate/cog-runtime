4a6fc1fd4f80127da7735bbde51feb984fbc6b14
Morgan Fainberg <code@tempusfrang.it>
2025-08-18 14:05:45 -0700

    Test Harness Retrofit: `path_out_test.go`
    Retrofit `path_out_test.go` for the new test harness.

--- internal/tests/path_out_test.go
+++ internal/tests/path_out_test.go
@@ -1,128 +1,114 @@
 package tests
 
 import (
+	"encoding/json"
+	"io"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/replicate/cog-runtime/internal/server"
 )
 
-// func TestPathOut(t *testing.T) {
-// 	testCases := []struct {
-// 		predictor     string
-// 		nested        bool
-// 		skipLegacyCog bool
-// 	}{
-// 		{
-// 			predictor:     "path_out",
-// 			nested:        false,
-// 			skipLegacyCog: false,
-// 		},
-// 		{
-// 			predictor: "path_out_any",
-// 			nested:    true,
-// 		},
-// 		{
-// 			predictor:     "path_out_dataclass",
-// 			nested:        true,
-// 			skipLegacyCog: true,
-// 		},
-// 		{
-// 			predictor:     "path_out_json",
-// 			nested:        true,
-// 			skipLegacyCog: false,
-// 		},
-// 		{
-// 			predictor:     "path_out_output",
-// 			nested:        true,
-// 			skipLegacyCog: false,
-// 		},
-// 		{
-// 			predictor:     "path_out_pathlike",
-// 			nested:        false,
-// 			skipLegacyCog: true,
-// 		},
-// 		{
-// 			predictor:     "path_out_pydantic",
-// 			nested:        true,
-// 			skipLegacyCog: false,
-// 		},
-// 	}
-// 	for _, testCase := range testCases {
-// 		t.Run(testCase.predictor, func(t *testing.T) {
-// 			testPathOut(t, testCase.predictor, testCase.nested)
-// 		})
-// 	}
-// }
-
-func testPathOut(t *testing.T, predictor string, nested bool) {
-	ct := NewCogTest(t, predictor)
-	assert.NoError(t, ct.Start())
-
-	hc := ct.WaitForSetup()
-	assert.Equal(t, server.StatusReady.String(), hc.Status)
-	assert.Equal(t, server.SetupSucceeded, hc.Setup.Status)
-
-	resp := ct.Prediction(map[string]any{"s": "foo"})
-	var b64 string
-	if *legacyCog {
-		// Compat: different MIME type detection logic
-		b64 = b64encodeLegacy("*foo*")
-	} else {
-		b64 = b64encode("*foo*")
-	}
-	var output any
-	if nested {
-		output = map[string]any{"p": b64}
-	} else {
-		output = b64
-	}
-	ct.AssertResponse(resp, server.PredictionSucceeded, output, "")
-
-	ct.Shutdown()
-	assert.NoError(t, ct.Cleanup())
-}
-
 func TestPathOut(t *testing.T) {
-	// Output type is Path
-	testPathOut(t, "path_out", false)
-}
+	b64Data := b64encode("*foo*")
+	b64LegacyData := b64encodeLegacy("*foo*")
 
-func TestPathOutAny(t *testing.T) {
-	// Output type is Any
-	testPathOut(t, "path_out_any", true)
-}
-
-func TestPathOutDataclass(t *testing.T) {
-	// Compat: legacy Cog does not support dataclass
-	if *legacyCog {
-		t.SkipNow()
+	testCases := []struct {
+		predictor     string
+		nested        bool
+		skipLegacyCog bool
+	}{
+		// Output type is Path
+		{
+			predictor:     "path_out",
+			nested:        false,
+			skipLegacyCog: false,
+		},
+		// Output type is Any
+		{
+			predictor: "path_out_any",
+			nested:    true,
+		},
+		// Output type is dataclass
+		{
+			predictor:     "path_out_dataclass",
+			nested:        true,
+			skipLegacyCog: true,
+		},
+		// Output type is dict[str, Any]
+		{
+			predictor:     "path_out_json",
+			nested:        true,
+			skipLegacyCog: false,
+		},
+		// Output type is cog.Output
+		{
+			predictor:     "path_out_output",
+			nested:        true,
+			skipLegacyCog: false,
+		},
+		// Output type is os.PathLike
+		{
+			predictor:     "path_out_pathlike",
+			nested:        false,
+			skipLegacyCog: true,
+		},
+		// Output type is a Pydantic base model
+		{
+			predictor:     "path_out_pydantic",
+			nested:        true,
+			skipLegacyCog: false,
+		},
 	}
-	// Output type is a dataclass
-	testPathOut(t, "path_out_dataclass", true)
-}
+	for _, testCase := range testCases {
+		t.Run(testCase.predictor, func(t *testing.T) {
+			t.Parallel()
+			if testCase.skipLegacyCog && *legacyCog {
+				t.Skipf("skipping %s due to legacy Cog configuration", testCase.predictor)
+			}
+			runtimeServer := setupCogRuntimeServer(t, cogRuntimeServerConfig{
+				procedureMode:    false,
+				explicitShutdown: false,
+				uploadURL:        "",
+				module:           testCase.predictor,
+				predictorClass:   "Predictor",
+			})
+			hc := waitForSetupComplete(t, runtimeServer)
+			assert.Equal(t, server.StatusReady.String(), hc.Status)
+			assert.Equal(t, server.SetupSucceeded, hc.Setup.Status)
 
-func TestPathOutJSON(t *testing.T) {
-	// Output type is dict[str, Any]
-	testPathOut(t, "path_out_json", true)
-}
+			prediction := server.PredictionRequest{Input: map[string]any{"s": "foo"}}
+			req := httpPredictionRequest(t, runtimeServer, nil, prediction)
+			resp, err := http.DefaultClient.Do(req)
+			require.NoError(t, err)
+			defer resp.Body.Close()
+			assert.Equal(t, http.StatusOK, resp.StatusCode)
+			body, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			var predictionResponse server.PredictionResponse
+			err = json.Unmarshal(body, &predictionResponse)
+			require.NoError(t, err)
+			assert.Equal(t, server.PredictionSucceeded, predictionResponse.Status)
 
-func TestPathOutOutput(t *testing.T) {
-	// Output type is cog.Output
-	testPathOut(t, "path_out_output", true)
-}
-
-func TestPathOutPathLike(t *testing.T) {
-	// Compat: legacy Cog does not support PathLike
-	if *legacyCog {
-		t.SkipNow()
+			b64 := b64Data
+			if *legacyCog {
+				b64 = b64LegacyData
+			}
+			if testCase.nested {
+				assert.Len(t, predictionResponse.Output, 1)
+				outputRaw, exists := predictionResponse.Output.(map[string]any)
+				require.True(t, exists, "output is not a map[string]any")
+				output, ok := outputRaw["p"].(string)
+				require.True(t, ok, "output is not a string")
+				assert.Equal(t, b64, output)
+			} else {
+				output, ok := predictionResponse.Output.(string)
+				require.True(t, ok, "output is not a string")
+				assert.Equal(t, b64, output)
+			}
+		})
 	}
-	// Output type is os.PathLike
-	testPathOut(t, "path_out_pathlike", false)
-}
-
-func TestPathOutPydantic(t *testing.T) {
-	// Output type is a Pydantic base model
-	testPathOut(t, "path_out_pydantic", true)
 }
