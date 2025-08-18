eee250b85d5e6489ff016e450b2bc61dd3c56f24
Neville Li <neville.lyh@gmail.com>
2025-08-18 16:57:27 -0400

    Delete dead code

--- python/tests/bad_predictors/input_default_none.py
+++ python/tests/bad_predictors/input_default_none.py
@@ -28,20 +28,3 @@ class Predictor(BasePredictor):
         i4c: int | None = Input(default=1),
     ) -> str:
         return 'foo'
-
-
-def predict(
-    i1a: int,
-    i1b: Optional[int],
-    i1c: int | None,
-    i2a: int = Input(),
-    i2b: int = Input(default=None),  # Bad
-    i2c: int = Input(default=0),
-    i3a: Optional[int] = Input(),
-    i3b: Optional[int] = Input(default=None),
-    i3c: Optional[int] = Input(default=0),
-    i4a: int | None = Input(),
-    i4b: int | None = Input(default=None),
-    i4c: int | None = Input(default=1),
-) -> str:
-    return 'foo'
