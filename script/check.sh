0cc27149be57e2965d4b06000147b077d18d3a12
Morgan Fainberg <code@tempusfrang.it>
2025-08-20 16:24:49 -0700

    Revert GCI change
    GCI change should happen with the linting PR.

--- script/check.sh
+++ script/check.sh
@@ -5,16 +5,14 @@
 set -euo pipefail
 
 base_dir="$(git rev-parse --show-toplevel)"
-GCI_VERSION="v0.13.7"
 
 check_go() {
-    echo "Running in $base_dir, GCI_VERSION=$GCI_VERSION"
     cd "$base_dir"
     local="$(go list -m)"
     if [[ -z "${CI:-}" ]]; then
-        go run github.com/daixiang0/gci@$GCI_VERSION write --skip-generated -s standard -s default -s "prefix(github.com/replicate/cog-runtime)" .
+        go run golang.org/x/tools/cmd/goimports@latest -d -w -local "$local" .
     else
-        output="$(go run github.com/daixiang0/gci@$GCI_VERSION diff --skip-generated -s standard -s default -s "prefix(github.com/replicate/cog-runtime)" .)"
+        output="$(go run golang.org/x/tools/cmd/goimports@latest -d -local "$local" .)"
         printf "%s" "$output"
         [ -z "$output" ] || exit 1
     fi
