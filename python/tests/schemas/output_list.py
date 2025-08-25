1889554b1470a85bf74cb9812ba278b42782158d
Will Sackfield <sackfield@replicate.com>
2025-08-25 15:58:40 -0400

    Add test for validating a list of Output objects

---
+++ python/tests/schemas/output_list.py
@@ -0,0 +1,22 @@
+from cog import BaseModel, BasePredictor
+from cog.coder import dataclass_coder
+
+
+class Output(BaseModel):
+    x: int
+    y: str
+
+
+FIXTURE = [
+    ({}, [Output(x=1, y='a'), Output(x=2, y='b'), Output(x=3, y='c')]),
+]
+
+
+class Predictor(BasePredictor):
+    setup_done = False
+
+    def setup(self) -> None:
+        self.setup_done = True
+
+    def predict(self) -> list[Output]:
+        return [Output(x=1, y='a'), Output(x=2, y='b'), Output(x=3, y='c')]
