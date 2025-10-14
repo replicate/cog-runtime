bc1e6083e451017e8dd403ff850ee1bfa67b4d3a
Nikhil Sinha <nikhil@replicate.com>
2025-10-13 17:02:33 -0700

    Remove testing sleep

--- internal/runner/manager.go
+++ internal/runner/manager.go
@@ -430,8 +430,6 @@ func (m *Manager) startRunnerFromCheckpoint(ctx context.Context, env []string, r
 		return nil, err
 	}
 
-	time.Sleep(10 * time.Minute)
-
 	runner, err := m.setupRunner(runtimeContext, runtimeCancel, cmd, env, runnerCtx, maxConcurrency)
 	if err != nil {
 		m.logger.Sugar().Errorw("failed to set up runner", "error", err)
